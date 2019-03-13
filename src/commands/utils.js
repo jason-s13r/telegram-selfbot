beda331b1d0861ccc8b0563c79ea261a53f2006c
Jason Schwarzenberger <jason@js.org.nz>
2019-03-14 00:02:05 +1300

    ...

--- src/commands/utils.js
+++ src/commands/utils.js
@@ -4,20 +4,37 @@ const { JASON, TANNER, DOMOTHY, HORSESHOE, SANDBOX } = require('../constants');
 const { sendMessage, getMessageDetails } = require('../io');
 const { handshake } = require('../handshake');
 
+const reverse = s => s.split('').reverse().join('');
+
 module.exports = function(message, update, client) {
   return function(cli, options) {
-    cli.command('prepend [words...]').action(function(args, cb = () => {}) {
-      const stdin = (args.stdin || []).join(' ');
-      const words = (args.words || []).join(' ');
-      this.log(words + stdin);
-      cb();
-    });
+    cli
+      .command('prepend [words...]')
+      .option('-u, --undo')
+      .action(function(args, cb = () => {}) {
+        const stdin = (args.stdin || []).join(' ');
+        const words = (args.words || []).join(' ');
+        if (args.options.undo) {
+          this.log(stdin.replace(words, ''));
+        } else {
+          this.log(words + stdin);
+        }
+        cb();
+      });
 
-    cli.command('append [words...]').action(function(args, cb = () => {}) {
-      const stdin = (args.stdin || []).join(' ');
-      const words = (args.words || []).join(' ');
-      this.log(stdin + words);
-      cb();
-    });
+    cli
+      .command('append [words...]')
+      .option('-u, --undo')
+      .action(function(args, cb = () => {}) {
+        const stdin = (args.stdin || []).join(' ');
+        const words = (args.words || []).join(' ');
+        if (args.options.undo) {
+
+          this.log(reverse(reverse(stdin).replace(reverse(words), '')));
+        } else {
+          this.log(stdin + words);
+        }
+        cb();
+      });
   };
 };
